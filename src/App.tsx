66385db532317fc926c3736f95c71cf1de7016c6
Carlosjw <carlos.ti.infor@gmail.com>
2025-05-22 15:20:52 -0300

    endLesson25

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Container } from './components/Container'
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
 import { CountDown } from './components/CountDown'
+import { CirclePlay } from 'lucide-react'
 
 export function App() {
     return (
@@ -17,6 +18,27 @@ export function App() {
             </Container>
             <Container>
                 <CountDown />
+            </Container>
+            <Container>
+                <form action="" className="form">
+                    <div className="formRow">
+                        <label htmlFor="inputTask">task</label>
+                        <input type="text" id="inputTask" />
+                    </div>
+
+                    <div className="formRow">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className="formRow">
+                        <p>Ciclos</p>
+                        <p>0 0 0 0 0 0 0 0</p>
+                    </div>
+
+                    <div className="formRow">
+                        <button className='sendButton'><CirclePlay/></button>
+                    </div>
+                </form>
             </Container>
 
         </>
