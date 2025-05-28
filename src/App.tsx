c4179b616516163631a51f7d924dbe61e723b275
Carlosjw <carlos.ti.infor@gmail.com>
2025-05-28 10:42:44 -0300

    endLesson41

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import './styles/theme.css'
 import './styles/global.css'
 
 import { Home } from './pages/Home'
-import { NotFound } from './pages/NotFound'
 
 export function App() {
-    return <NotFound />
+    return <Home />
 }
 
